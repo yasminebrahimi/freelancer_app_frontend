740034395509ebdd8f96e3e91f1beb70cfad1695
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-06-26 15:51:32 +0300

    Add Tailwind color tokens & base styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl bg-opacity-80 text-error bg-primary-900">
+      Hello world!
+    </h1>
+  );
 }
 
 export default App;
