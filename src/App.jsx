0310d13d152dcb6f56763187efa4e230356745d0
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-06-26 16:10:05 +0300

    Add OTP auth implementation notes

--- src/App.jsx
+++ src/App.jsx
@@ -9,3 +9,15 @@ function App() {
 }
 
 export default App;
+
+
+// auth 
+// Task: #1: auth user via OTP : one-time-password
+// 1. form -> getOTP -> input -> button => phoneNumber => send OTP
+// 2. form -> checkOTP -> request -> (OTP, phoneNumber)
+
+
+// request => 
+  // 1. axios (useState, useEffect)
+// 2. useFetch (data, loading, error)
+// 3. react-query => redux alrernative (remote states). fetch (get). mutate (post)
