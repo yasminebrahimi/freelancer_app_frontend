c95fd6fc573e724301b07ec1bab872c7a8de6bc1
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-01 12:21:24 +0300

    Rename and fix CheckOTPForm component

--- src/features/authentication/checkOTPForm.jsx
+++ src/features/authentication/checkOTPForm.jsx
@@ -1,32 +1,31 @@
 import React, { useState } from "react";
 import OTPInput from "react-otp-input";
 
-function checkOTPForm() {
+function CheckOTPForm() {
   const [otp, setOtp] = useState("");
   return (
     <div>
       <form className="space-y-10">
-        <p className="font-bold text-secondary-800">
-          Enter the verification code
-          <OTPInput
-            value="otp"
-            onChange={setOtp}
-            numInputs={6}
-            renderSeparator={<span>-</span>}
-            renderInput={(props) => <input type="number" {...props} />}
-            containerStyle="felx flex-row-reverse gap-x-2 justify-center"
-            inputStyle={{
-              width: "2.5rem",
-              padding: "0.5rem 0.2rem",
-              border: "1px solid rgb(var(--color--primary-300))",
-              borderRadius: "0.5rem",
-            }}
-          />
-          <button className="btn btn--primary w-full"> Verify </button>
-        </p>
+        <p className="font-bold text-secondary-800"></p>
+        Enter the verification code
+        <OTPInput
+          value="otp"
+          onChange={setOtp}
+          numInputs={6}
+          renderSeparator={<span>-</span>}
+          renderInput={(props) => <input type="number" {...props} />}
+          containerStyle="felx flex-row-reverse gap-x-2 justify-center"
+          inputStyle={{
+            width: "2.5rem",
+            padding: "0.5rem 0.2rem",
+            border: "1px solid rgb(var(--color--primary-300))",
+            borderRadius: "0.5rem",
+          }}
+        />
+        <button className="btn btn--primary w-full"> Verify </button>
       </form>
     </div>
   );
 }
 
-export default checkOTPForm;
+export default CheckOTPForm;
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import SendOTPForm from "../features/authentication/sendOIPFrom";
-import checkOTPForm from "../features/authentication/checkOTPForm";
+import SendOTPForm from "../features/authentication/SendOIPFrom";
+import CheckOTPForm from "../features/authentication/CheckOTPForm";
 
 function Auth() {
   return (
     <div className="flex justify-center pt-10">
       <div className="w-full sm:max-w-sm">
         {/* <SendOTPForm /> */}
-        <checkOTPForm />
+        <CheckOTPForm />
       </div>
     </div>
   );
