00da59df48c2a71543d4404e8c4e34b1091703d0
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-06 17:05:35 +0300

    Install React Query and fix SendOTP form submission

--- src/features/authentication/sendOIPFrom.jsx
+++ src/features/authentication/sendOIPFrom.jsx
@@ -3,16 +3,21 @@ import TextField from "../../ui/TextField";
 
 function SendOTPForm() {
   const [phoneNumber, setPhoneNumber] = useState("");
+
+  const sendOtpHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <form className="space-y-8">
+      <form className="space-y-8" onSubmit={sendOtpHandler}>
         <TextField
           label="Phone Number"
           name="phoneNumber"
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
         />
-        <button className="btn btn--primary w-full">
+        <button type="submit" className="btn btn--primary w-full">
           Send verification code
         </button>
       </form>
