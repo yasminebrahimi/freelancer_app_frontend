a1a8f0b6133e7ea3e158c590df5643ff6f892049
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-06 17:00:39 +0300

    Add HTTP and auth services with axios

---
+++ src/services/authService.js
@@ -0,0 +1,10 @@
+import http from "./httpService";
+
+export function getOtp(data) {
+  //{phoneNumber: "0400000000"}
+  return http.post("/user/get-otp", data);
+}
+
+export function checkOtp(data) {
+  return http.post("/user/check-otp", data);
+}
---
+++ src/services/httpService.js
@@ -0,0 +1,14 @@
+axios.create({
+  baseURL: "http://localhost:5001/api",
+  withCredentials: true,
+});
+
+const http = {
+  get: app.get,
+  post: app.post,
+  delete: app.delete,
+  put: app.put,
+  patch: app.patch,
+};
+
+export default http;
