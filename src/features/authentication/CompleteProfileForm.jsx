65cdfd6ce7b89323780454a05e6f1b215a0c7a5d
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-10 14:44:28 +0300

    Extract RadioInput component

--- src/features/authentication/CompleteProfileForm.jsx
+++ src/features/authentication/CompleteProfileForm.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import TextField from "../../ui/TextField";
+import RadioInput from "../../ui/RadioInput";
 
 function CompleteProfileForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+
   return (
     <div className="flex justify-center pt-10">
       <div className="w-full sm:max-w-sm">
@@ -21,17 +24,24 @@ function CompleteProfileForm() {
             value={email}
           />
           <div className="flex items-center justify-center gap-x-8">
-            <div className="flex items-center gap-x-2 text-secondary-600">
-              <input
-                className="cursor-pointer w-4 h-4 form-radio text-primary-900 focus:ring-blue-500"
-                type="radio"
-                name="role"
-                id="OWNER"
-                value="OWNER"
-              />
-              <label htmlFor="OWNER">Owner</label>
-            </div>
-            <div className="flex items-center justify-center gap-x-1">
+            <RadioInput
+              label="owner"
+              value="OWNER"
+              onChange={(e) => setRole(e.target.value)}
+              id="OWNER"
+              name="role"
+              checked={role === "OWNER"}
+            />
+            <RadioInput
+              label="freelancer"
+              value="FREELANCER"
+              onChange={(e) => setRole(e.target.value)}
+              id="FREELANCER"
+              name="role"
+              checked={role === "FREELANCER"}
+            />
+
+            {/* <div className="flex items-center justify-center gap-x-1">
               <input
                 className="cursor-pointer w-4 h-4 form-radio text-primary-900 focus:ring-blue-500"
                 type="radio"
@@ -40,7 +50,8 @@ function CompleteProfileForm() {
                 value="FREELANCER"
               />
               <label htmlFor="FREELANCER">Freelancer</label>
-            </div>
+            </div> */}
+
             <div></div>
           </div>
           <button className="btn btn--primary w-full">Verify</button>
