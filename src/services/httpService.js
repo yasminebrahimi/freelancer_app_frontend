cb50c2ab54ab8e5d9985659054294d6c076e9618
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-14 10:51:10 +0300

    Add token refresh interceptor to http service

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,9 +1,40 @@
 import axios from "axios";
 
+const BASE_URL = "http://localhost:5001/api";
+
 const app = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
+
+// request => error
+// response => err
+
+app.interceptors.request.use(
+  (res) => res,
+  (err) => Promise.reject(err),
+);
+
+// err => response => status === 401 => proccess => !!!!
+app.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalConfig = err.config;
+
+    if (err.response?.status === 401) {
+      try {
+        const { data } = await axios.get(`${BASE_URL}/user/refresh-token`, {
+          withCredentials: true,
+        });
+
+        if (data) return app(originalConfig);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(err);
+  },
+);
 
 const http = {
   get: app.get,
