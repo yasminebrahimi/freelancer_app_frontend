47c1757150716659fb4939e0e43029792b6e4235
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-14 11:56:59 +0300

    Implement grid layout with React Router outlet

--- src/ui/AppLauout.jsx
+++ src/ui/AppLauout.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 function AppLauout() {
   return (
-    <div>
-      <div className="">app header</div>
-      <div className="">app sidebar</div>
-      <div className="">app content</div>
+    <div className="grid h-screen grid-rows-[auto-1fr] grid-cols-[15rem_1fr]">
+      <div className="bg-red-100 col-span-2">app header</div>
+      <div className="bg-red-200">app sidebar</div>
+      <div className="bg-red-300">
+        <Outlet />
+      </div>
     </div>
   );
 }
