46dea0f970d5c36f71f4c1abca6d38e78918afb4
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-14 15:44:19 +0300

    Add useUser hook to Header component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import useUser from "../features/authentication/useUser";
 
 function Header() {
+  const { data } = useUser();
+
   return <div className="bg-secondary-0 py-4 px-8">app header</div>;
 }
 
