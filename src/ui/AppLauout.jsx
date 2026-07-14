26db08071a06a86db6c987b833dbd6bef889969f
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-14 12:17:22 +0300

    Refactor AppLayout styling and grid layout

--- src/ui/AppLauout.jsx
+++ src/ui/AppLauout.jsx
@@ -4,10 +4,12 @@ import { Outlet } from "react-router-dom";
 function AppLauout() {
   return (
     <div className="grid h-screen grid-rows-[auto-1fr] grid-cols-[15rem_1fr]">
-      <div className="bg-red-100 col-span-2">app header</div>
-      <div className="bg-red-200">app sidebar</div>
-      <div className="bg-red-300">
-        <Outlet />
+      <div className="bg-secondary-0 py-4 px-8">app header</div>
+      <div className="bg-secondary-0 row-start-1 row-span-2">app sidebar</div>
+      <div className="bg-secondary-100 p-8 overflow-y-auto">
+        <div className="mx-auto max-w-screen-md bg-red-300">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
