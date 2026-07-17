d7ea2d7d98881c803fede7c0e5edf8bedac3a58e
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-17 19:11:35 +0300

    Sidebar: add NavLinks with icons and active styles

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import { HiCollection, HiHome } from "react-icons/hi";
+import { NavLink } from "react-router-dom";
+
+const navlinkClass =
+  "flex items-center gap-x-2 hover:bg-primary-100/50 hover:text-primary-900 px-2 py-1.5 rounded-lg text-secondary-600 transition-all duration-300";
 
 function Sidebar() {
   return (
-    <div className="bg-secondary-0 row-start-1 row-span-2">app sidebar</div>
+    <div className="bg-secondary-0 row-start-1 row-span-2">
+      <ul className="flex flex-col gap-y-4">
+        <li>
+          <NavLink
+            to="/owner/dashboard"
+            className={({ isActive }) =>
+              isActive
+                ? `${navlinkClass} bg-primary-100/50 text-primary-900`
+                : `${navlinkClass} text-secondary-600`
+            }
+          >
+            <HiHome />
+            <span>Home</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/owner/projects"
+            className={({ isActive }) =>
+              isActive
+                ? `${navlinkClass} bg-primary-100/50 text-primary-900`
+                : `${navlinkClass} text-secondary-600`
+            }
+          >
+            <HiCollection />
+            <span>Projects</span>
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
 }
 
