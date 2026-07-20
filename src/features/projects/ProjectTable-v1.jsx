cf7a13c67415df7245862a749519f7966dd5c75c
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-20 10:10:53 +0300

    Refactor ProjectTable to use Table component

---
+++ src/features/projects/ProjectTable-v1.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Loading from "../../ui/Loading";
+import useOwnerProjects from "./useOwnerProjects";
+import truncateText from "../../utils/truncateText";
+import toLocalDateShort from "../../utils/toLocalDateShort";
+
+function ProjectTable() {
+  const { isLoading, projects } = useOwnerProjects();
+
+  if (!projects.length) return <Empty resourceName="project" />;
+
+  return (
+    <div className="bg-secondary-0 overflow-x-auto ">
+      <table>
+        <thead>
+          <tr className="title-row">
+            <th>#</th>
+            <th>name</th>
+            <th>category</th>
+            <th>budget</th>
+            <th>deadline</th>
+            <th>tags</th>
+            <th>freelancer</th>
+            <th>situation</th>
+            <th>activities</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((project, index) => (
+            <tr key={project.id}>
+              <td>{index + 1}</td>
+              <td>{truncateText(project.title, 30)}</td>
+              <td>{project.category.title}</td>
+              <td>{project.budget}</td>
+              <td>{toLocalDateShort(project.deadline)}</td>
+
+              <td>
+                <div className="felx felx-wrap items-center gap-2 max-w-[200px]">
+                  {project.tag.map((tag) => (
+                    <span className="badge badge--secondary" key={tag}>
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+              </td>
+
+              <td>{project.freelancer?.name || "-"}</td>
+              <td>
+                {project.status === "OPEN" ? (
+                  <span className="badge badge--success">Open</span>
+                ) : (
+                  <span className="badge badge--danger">Close</span>
+                )}
+              </td>
+              <td>....</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ProjectTable;
--- src/features/projects/ProjectTable.jsx
+++ src/features/projects/ProjectTable.jsx
@@ -10,54 +10,51 @@ function ProjectTable() {
   if (!projects.length) return <Empty resourceName="project" />;
 
   return (
-    <div className="bg-secondary-0 overflow-x-auto ">
-      <table>
-        <thead>
-          <tr className="title-row">
-            <th>#</th>
-            <th>name</th>
-            <th>category</th>
-            <th>budget</th>
-            <th>deadline</th>
-            <th>tags</th>
-            <th>freelancer</th>
-            <th>situation</th>
-            <th>activities</th>
+    <Table>
+      <Table.Header>
+        <th>#</th>
+        <th>name</th>
+        <th>category</th>
+        <th>budget</th>
+        <th>deadline</th>
+        <th>tags</th>
+        <th>freelancer</th>
+        <th>situation</th>
+        <th>activities</th>
+      </Table.Header>
+
+      <Table.Body>
+        {projects.map((project, index) => (
+          <tr key={project.id}>
+            <td>{index + 1}</td>
+            <td>{truncateText(project.title, 30)}</td>
+            <td>{project.category.title}</td>
+            <td>{project.budget}</td>
+            <td>{toLocalDateShort(project.deadline)}</td>
+
+            <td>
+              <div className="felx felx-wrap items-center gap-2 max-w-[200px]">
+                {project.tag.map((tag) => (
+                  <span className="badge badge--secondary" key={tag}>
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </td>
+
+            <td>{project.freelancer?.name || "-"}</td>
+            <td>
+              {project.status === "OPEN" ? (
+                <span className="badge badge--success">Open</span>
+              ) : (
+                <span className="badge badge--danger">Close</span>
+              )}
+            </td>
+            <td>....</td>
           </tr>
-        </thead>
-        <tbody>
-          {projects.map((project, index) => (
-            <tr key={project.id}>
-              <td>{index + 1}</td>
-              <td>{truncateText(project.title, 30)}</td>
-              <td>{project.category.title}</td>
-              <td>{project.budget}</td>
-              <td>{toLocalDateShort(project.deadline)}</td>
-
-              <td>
-                <div className="felx felx-wrap items-center gap-2 max-w-[200px]">
-                  {project.tag.map((tag) => (
-                    <span className="badge badge--secondary" key={tag}>
-                      {tag}
-                    </span>
-                  ))}
-                </div>
-              </td>
-
-              <td>{project.freelancer?.name || "-"}</td>
-              <td>
-                {project.status === "OPEN" ? (
-                  <span className="badge badge--success">Open</span>
-                ) : (
-                  <span className="badge badge--danger">Close</span>
-                )}
-              </td>
-              <td>....</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+        ))}
+      </Table.Body>
+    </Table>
   );
 }
 
