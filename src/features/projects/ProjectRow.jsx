f99c7ce9e0295a00226d0c02cfd27844362092f3
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-20 11:45:03 +0300

    Add edit/delete actions and Modal component to ProjectRow

--- src/features/projects/ProjectRow.jsx
+++ src/features/projects/ProjectRow.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import toLocalDateShort from "../../utils/toLocalDateShort";
 import truncateText from "../../utils/truncateText";
+import { HiOutlineTrash } from "react-icons/hi";
+import { TbPencilMinus } from "react-icons/tb";
 
 function ProjectRow({ project, index }) {
   return (
@@ -29,7 +31,17 @@ function ProjectRow({ project, index }) {
           <span className="badge badge--danger">Close</span>
         )}
       </td>
-      <td>....</td>
+      <td>
+        <div className="flex items-center gap-x-4">
+          <button>
+            <TbPencilMinus className="w-5 h-5 text-primary-900" />
+          </button>
+          <Model open={true}>this is modal...</Model>
+          <button>
+            <HiOutlineTrash className="w-5 h-5 text-error" />
+          </button>
+        </div>
+      </td>
     </Table.Row>
   );
 }
---
+++ src/utils/Modal.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { HiOutlineX } from "react-icons/hi";
+
+function Modal({ open, onClose, title, children }) {
+  return (
+    open && (
+      <div
+        className="backdrop-blur-sm fixed top-0 left-0
+  w-full h-screen bg-secondary-800 bg-opacity-30 z-50"
+      >
+        <div
+          className="fixed top-1/2 left-1/2 translate-x-1/2 translate-y-1/2
+        rounded-lg bg-secondary-0 p-4 shadow-lg transition-all duration-500 ease-out
+        w-64"
+        >
+          <div className="flex items-center justify-between border-b border-b-secondary-300 pb-2 mb-6">
+            <p className="text-secondary-700 font-bold text-base">
+              model title
+            </p>
+            <button>
+              <HiOutlineX className="w-5 h-5 text-secondary-500" />
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  );
+}
+
+export default Modal;
