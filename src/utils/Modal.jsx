f0161b539800a52199d736b4d516c3aac4598603
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-21 11:03:17 +0300

    Improve modal responsive design and overflow

--- src/utils/Modal.jsx
+++ src/utils/Modal.jsx
@@ -11,7 +11,7 @@ function Modal({ open, onClose, title, children }) {
         <div
           className="fixed top-1/2 left-1/2 translate-x-1/2 translate-y-1/2
         rounded-lg bg-secondary-0 p-4 shadow-lg transition-all duration-500 ease-out
-        w-[calc(100vw-1rem)]"
+        w-[calc(100vw-3rem)] md:max-w-lg max-h-[calc(100vh-2rem)] overflow-auto"
         >
           <div className="flex items-center justify-between border-b border-b-secondary-300 pb-2 mb-6">
             <p className="text-secondary-700 font-bold text-base">{title}</p>
