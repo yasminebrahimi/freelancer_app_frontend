45acf5dcd9d3e7151f2583ab93ff2bfc54abb7f0
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-22 17:40:48 +0300

    Add click-outside handler to Modal

--- src/utils/Modal.jsx
+++ src/utils/Modal.jsx
@@ -2,13 +2,23 @@ import React from "react";
 import { HiOutlineX } from "react-icons/hi";
 
 function Modal({ open, onClose, title, children }) {
+  const ref = useRef();
+
+  useEffect(() => {
+    function handleClick(e) {
+      if(ref.current && )
+    }
+    document.addEventListener("click", handleClick);
+  }, []);
+
   return (
     open && (
       <div
         className="backdrop-blur-sm fixed top-0 left-0
-  w-full h-screen bg-secondary-800 bg-opacity-30 z-50"
+            w-full h-screen bg-secondary-800 bg-opacity-30 z-50"
       >
         <div
+          ref={ref}
           className="fixed top-1/2 left-1/2 translate-x-1/2 translate-y-1/2
         rounded-lg bg-secondary-0 p-4 shadow-lg transition-all duration-500 ease-out
         w-[calc(100vw-3rem)] md:max-w-lg max-h-[calc(100vh-2rem)] overflow-auto"
